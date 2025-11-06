c9f317f89147c0bf5a87592467aec54c4e708783
Polisetty Sairam <21jr1a12h2@gmail.com>
2025-11-06 12:58:50 +0530

    Update TestClassMain.java

--- MyCodeProject/src/com/test/demo/TestClassMain.java
+++ MyCodeProject/src/com/test/demo/TestClassMain.java
@@ -3,8 +3,10 @@ package com.test.demo;
 public class TestClassMain {
 
 	public static void main(String[] args) {
+		System.out.println("Time is 1 oo clock");
 		// TODO Auto-generated method stub
 
 	}
 
 }
+
